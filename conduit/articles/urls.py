86cf2921fc532b7215b99bccaef44cef9cb99bdc
gm-willo <gianmarco.ninno@willo.it>
2026-02-17 17:28:20 +0100

    Add url/view/template for create, edit and delete articles

--- conduit/articles/urls.py
+++ conduit/articles/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("article/<slug:slug>-<uuid:uuid>", views.article_detail, name="article_detail")
+    path("article/<slug:slug>-<uuid:uuid>", views.article_detail, name="article_detail"),
+    path("editor/", views.article_create, name="article_create"),
+    path("editor/<slug:slug>-<uuid:uuid>", views.article_update, name="article_update"),
+    path("editor/<slug:slug>-<uuid:uuid>/delete", views.article_delete, name="article_delete"),
 ]
--- conduit/articles/views.py
+++ conduit/articles/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden, HttpResponse
+
+from conduit.articles.forms import ArticleForm
 
 from .models import Article
 
@@ -17,3 +22,59 @@ def article_detail(request, slug, uuid):
     article = get_object_or_404(Article, slug=slug, uuid=uuid)
     context = {"article": article}
     return render(request, "article_detail.html", context)
+
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def article_create(request):
+    """View for creating articles."""
+    
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user.profile
+            article.save()
+            return redirect(article.get_absolute_url())
+    else:
+        form = ArticleForm()
+    return render(request, "editor.html", {"form": form})
+
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def article_update(request, slug, uuid):
+    """View for editing articles."""
+    
+    article = get_object_or_404(Article, slug=slug, uuid=uuid)
+    
+    if article.author != request.user.profile:
+        return HttpResponseForbidden("You've not the permission to modify this article.")
+    
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        
+        if form.is_valid():
+            form.save()
+            return redirect(article.get_absolute_url())
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, "editor.html", {"form": form, "article": article})
+
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def article_delete(request, slug, uuid):
+    """View for deleting articles."""
+    
+    article = get_object_or_404(Article, slug=slug, uuid=uuid)
+    
+    if article.author != request.user.profile:
+        return HttpResponseForbidden("You've not the permission to delete this article.")
+    
+    if request.method == "POST":
+        article.delete()
+        return redirect("home")
+    
+    return render(request, "article_detail.html", {"article": article})
