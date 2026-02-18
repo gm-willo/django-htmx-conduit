7f82875a8762e2f824253053f4f0e7cd9a1b7de1
gm-willo <gianmarco.ninno@willo.it>
2026-02-18 14:06:19 +0100

    Add authentication functionalities with login, logout and signup

--- conduit/users/views.py
+++ conduit/users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
 
-# Create your views here.
+
+class Login(LoginView):
+    template_name = "login.html"
+    next_page = reverse_lazy("home")
+    
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(self.next_page)
+        return super().get(request, *args, **kwargs)
+    
+
+class Logout(LogoutView):
+    next_page = reverse_lazy("home")
+
+
+class SignUpView(CreateView):
+    model = get_user_model()
+    fields = ["username", "email", "password"]
+    template_name = "signup.html"
+    success_url = reverse_lazy("home")
+    
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(self.success_url)
+        return super().get(request, *args, **kwargs)
