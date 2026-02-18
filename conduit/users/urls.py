2d175728df2414358bd7ad3885a72e52a9b4bc4a
gm-willo <gianmarco.ninno@willo.it>
2026-02-18 16:38:55 +0100

    Add profile features

--- conduit/users/urls.py
+++ conduit/users/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from .views import Login, Logout, SignUpView
 
+from . import views
+
 
 urlpatterns = [
     path("login", Login.as_view(), name="login"),
     path("logout", Logout.as_view(), name="logout"),
     path("signup", SignUpView.as_view(), name="signup"),
+    path("profile/@<str:username>", views.profile_detail, name="profile_detail")
 ]
--- conduit/users/views.py
+++ conduit/users/views.py
@@ -1,26 +1,29 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render, get_object_or_404
+from django.http import HttpResponseForbidden
+
+from .models import User
 
 
 class Login(LoginView):
+    """Class based view for user login"""
     template_name = "login.html"
     next_page = reverse_lazy("home")
-    
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect(self.next_page)
-        return super().get(request, *args, **kwargs)
+    redirect_authenticated_user = True
     
 
 class Logout(LogoutView):
+    """Class based view for user logout"""
     next_page = reverse_lazy("home")
 
 
 class SignUpView(CreateView):
+    """Class based view for user signup"""
     model = get_user_model()
     fields = ["username", "email", "password"]
     template_name = "signup.html"
@@ -41,4 +44,18 @@ class SignUpView(CreateView):
         authenticated_user = authenticate(email=email, password=password)
         login(self.request, authenticated_user)
         return redirect(self.success_url)
+
+
+def profile_detail(request, username):
+    """Detail view for user profiles"""
     
+    user = get_object_or_404(User, username=username)
+    profile = user.profile
+    context = {"profile": profile}
+    
+    if request.user.is_authenticated:
+        context["my_articles"] = profile.articles.order_by("-created_at")
+    
+    return render(request, "profile_detail.html", context)
+
+
